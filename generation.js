ed4bf656af836e839769fca736ace3730a786d45
levelfourtwenty <level420@protonmail.com>
2022-01-19 01:55:17 +0000

    Added footer generation script

---
+++ generation.js
@@ -0,0 +1,4 @@
+function footergen() {
+document.write("<footer><nav class=\"navbar navbar-expand-lg\"><ul class=\"navbar-nav\"><li class=\"nav-item\"><a class=\"nav-link\" href=\"/tos\"><small> Terms of Service </small></a></li><li class=\"nav-item\"><a class=\"nav-link\" href=\"/privacy\"><small>Privacy Policy</small></a></li><li class=\"nav-item\"><a class=\"nav-link\" href=\"/takedown\"><small>Takedown Requests</small></a></li><li class=\"nav-item\"><a class=\"nav-link\" href=\"/developers\"><small>Developer Credits</small></a></li></ul></nav></footer>");
+}
+
