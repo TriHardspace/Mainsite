55fa2489498f4e36927126ef5be134a53a5bb4eb
levelfourtwenty <65911884+levelfourtwenty@users.noreply.github.com>
2022-07-05 01:28:22 -0500

    Removed unneccessary alert.

--- clipboard.js
+++ clipboard.js
@@ -1,6 +1,5 @@
         function clipcopy() {
                 var site = document.getElementById("copyUrl").value
-                alert(site)
                 navigator.clipboard.writeText(site)
                 return site
                 }
