91492e6691a33d264050ccd877ca6d0cfe9ded19
levelfourtwenty <65911884+levelfourtwenty@users.noreply.github.com>
2022-07-05 01:38:22 -0500

    Update clipboard.js

--- clipboard.js
+++ clipboard.js
@@ -1,5 +1,8 @@
         function clipcopy() {
-                var site = document.getElementById("copyUrl").value
-                navigator.clipboard.writeText(site)
-                return site
+                var site = document.getElementById("copyUrl").value;
+                navigator.clipboard.writeText(site);
+                var popupelement = document.getElementById("copybutton");
+                var popover = new bootstrap.Popover(popupelement, content="URL Copied", placement="bottom", trigger="manual");
+                popover.show();
+                return site;
                 }
